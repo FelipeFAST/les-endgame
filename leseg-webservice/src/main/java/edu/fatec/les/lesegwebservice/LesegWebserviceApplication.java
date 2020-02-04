317a9a77513889e98e0217a2caa1a733faf1d932
FelipeFAST <alves_felipesilva@outlook.com>
2020-02-04 12:08:55 -0300

    Initial commit

---
+++ leseg-webservice/src/main/java/edu/fatec/les/lesegwebservice/LesegWebserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.fatec.les.lesegwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesegWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesegWebserviceApplication.class, args);
+	}
+
+}
---
+++ leseg-webservice/src/test/java/edu/fatec/les/lesegwebservice/LesegWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fatec.les.lesegwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesegWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
